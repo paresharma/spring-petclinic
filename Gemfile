ddd1a5e3b1e1bf100ff35cba3b6b5b9eeba3cef4
paresharma <paresh.brahm@gmail.com>
2015-09-22 17:25:34 +0200

    Add team rspec

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby File.read('.ruby-version').rstrip
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'poltergeist'
+  gem 'rspec-page-regression'
+  gem 'rspec'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'rspec/page-regression'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
+Capybara.app_host = 'http://localhost:8080'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+  config.warnings = true
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
